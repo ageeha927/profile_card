a9e6e4c4dd185ab41805241715d8738a8bd57247
ageeha927 <ageeha927@west-mec.org>
2024-12-07 22:49:08 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,13 @@
+<<<<<<< Updated upstream
 import ProfileCard from './components/ProfileCard';
 import Footer from './components/Footer';
 import Gallery from './components/Gallery';
+=======
+// import ProfileCard from './components/ProfileCard';
+// import Footer from './components/Footer';
+
+import Gallery from './components/Gallery'
+>>>>>>> Stashed changes
 
 function Test() {
     return (
